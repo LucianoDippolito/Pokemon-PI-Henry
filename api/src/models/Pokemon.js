08b13eeb1fe82d02b0fc05dd1f1b16b1adb4f43c
Luciano <dippolitoluciano6@gmail.com>
2023-11-06 19:51:11 -0300

    Commit DB

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,75 @@
+require('dotenv').config();
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
+
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID, // Universally Unique Identifier garantiza IDs únicos y globales.
+      defaultValue: DataTypes.UUIDV4, // Genera UUIDs aleatorios (versión 4)
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        len: [0, 20]
+      }
     },
+    hp: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    height:{
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    weight:{
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 9999
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: false, 
   });
 };
---
+++ api/src/models/Type.js
@@ -0,0 +1,21 @@
+const { DataTypes, Sequelize } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+    sequelize.define('type', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+    },
+    {
+        timestamps: false,
+    });
+};
